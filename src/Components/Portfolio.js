d8801026f8660a60204a0856684693341322c587
Mark Brock <Markbrock@users.noreply.github.com>
2022-04-11 20:52:38 -0400

    link fix

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -9,7 +9,7 @@ export default function Portfolio() {
           {
             ProjectName: "Bug Tracker",
             About: "This is a Bug Tracker deployed on aws using cloudformation and cognito for authentication",
-            href: "http://d2ott0l3gydjik.cloudfront.net/",
+            href: "https://d2ott0l3gydjik.cloudfront.net/",
             siteMap: BugtrackerSiteMap,
             Github: "https://github.com/Markbrock/BugTracker"
           },
